37cc735a2d6104b059c45b4eaaadf6f12569e5b4
Lucas <lucasds.032@gmail.com>
2024-08-12 20:01:50 -0300

    Finished Orders Page

--- compra.js
+++ compra.js
@@ -1,6 +1,8 @@
 var data = window.location.search;
-var parametros = new URLSearchParams(data)
-var nome = parametros.get("nome")
-var cep = parametros.get("cep")
-var compradorTexto = $( "#OlaComprador" );
-compradorTexto.html("Olá "+nome+" confirme seu pedido")
+var parametros = new URLSearchParams(data);
+var nome = parametros.get("nome");
+var cep = parametros.get("cep");
+var compradorTexto = $("#OlaComprador");
+compradorTexto.html("Olá " + nome + " confirme seu pedido");
+var cepTexto = $("#Cep-confirmacao");
+cepTexto.html("Enviando o pedido para o endereço de Cep: " + cep);
